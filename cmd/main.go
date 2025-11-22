ca5009dad4e6d98363bc9f564742c9556328ea46
Melroy van den Berg <melroy@melroy.org>
2025-11-22 15:42:56 +0100

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	_ "github.com/melroy89/angie_telegraf_plugin/plugins/inputs/angie_api"
+
+	"github.com/influxdata/telegraf/plugins/common/shim"
+)
+
+var configFile = flag.String("config", "", "path to the config file for this plugin")
+var err error
+
+func main() {
+	// parse command line options
+	flag.Parse()
+
+	// create the shim. This is what will run your plugins.
+	s := shim.New()
+
+	// Check for settings from a config toml file,
+	err = s.LoadConfig(configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Err loading input: %s\n", err)
+		os.Exit(1)
+	}
+
+	// run the plugin until stdin closes or we receive a termination signal
+	if err := s.Run(shim.PollIntervalDisabled); err != nil {
+		fmt.Fprintf(os.Stderr, "Err: %s\n", err)
+		os.Exit(1)
+	}
+}
+
---
+++ plugins/inputs/angie_api/angie_api.go
@@ -0,0 +1,127 @@
+package angie_api
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
+	common_http "github.com/influxdata/telegraf/plugins/common/http"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+const sampleConfig = `
+# Read Angie API advanced status information
+[[inputs.angie_api]]
+  ## An array of Angie API URIs to gather stats.
+  urls = ["http://localhost/status"]
+  # Angie API version, default: 1
+  # api_version = 1
+
+  # HTTP response timeout (default: 5s)
+  response_timeout = "5s"
+
+  ## Optional TLS Config
+  # tls_ca = "/etc/telegraf/ca.pem"
+  # tls_cert = "/etc/telegraf/cert.pem"
+  # tls_key = "/etc/telegraf/key.pem"
+  ## Use TLS but skip chain & host verification
+  # insecure_skip_verify = false
+  `
+
+const (
+	// Default settings
+	defaultAPIVersion = 3
+
+	// Paths
+	processesPath   = "processes"
+	connectionsPath = "connections"
+	slabsPath       = "slabs"
+	sslPath         = "ssl"
+
+	httpRequestsPath      = "http/requests"
+	httpServerZonesPath   = "http/server_zones"
+	httpLocationZonesPath = "http/location_zones"
+	httpUpstreamsPath     = "http/upstreams"
+	httpCachesPath        = "http/caches"
+	httpLimitReqsPath     = "http/limit_reqs"
+	resolverZonesPath     = "resolvers"
+
+	streamServerZonesPath = "stream/server_zones"
+	streamUpstreamsPath   = "stream/upstreams"
+)
+
+type AngieAPI struct {
+	Urls       []string        `toml:"urls"`
+	APIVersion int64           `toml:"api_version"`
+	Log        telegraf.Logger `toml:"-"`
+	common_http.HTTPClientConfig
+
+	client *http.Client
+}
+
+func (*AngieAPI) SampleConfig() string {
+	return sampleConfig
+}
+
+func (n *AngieAPI) Gather(acc telegraf.Accumulator) error {
+	var wg sync.WaitGroup
+
+	// Create an HTTP client that is re-used for each
+	// collection interval
+
+	if n.APIVersion == 0 {
+		n.APIVersion = defaultAPIVersion
+	}
+
+	if n.client == nil {
+		client, err := n.createHTTPClient()
+		if err != nil {
+			return err
+		}
+		n.client = client
+	}
+
+	for _, u := range n.Urls {
+		addr, err := url.Parse(u)
+		if err != nil {
+			acc.AddError(fmt.Errorf("unable to parse address %q: %w", u, err))
+			continue
+		}
+
+		wg.Add(1)
+		go func(addr *url.URL) {
+			defer wg.Done()
+			n.gatherMetrics(addr, acc)
+		}(addr)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func (n *AngieAPI) createHTTPClient() (*http.Client, error) {
+	if n.HTTPClientConfig.ResponseHeaderTimeout < config.Duration(time.Second) {
+		n.HTTPClientConfig.ResponseHeaderTimeout = config.Duration(time.Second * 5)
+	}
+
+	// Create the client
+	ctx := context.Background()
+	client, err := n.HTTPClientConfig.CreateClient(ctx, n.Log)
+	if err != nil {
+		return nil, fmt.Errorf("creating client failed: %w", err)
+	}
+
+	return client, nil
+}
+
+func init() {
+	inputs.Add("nginx_plus_api", func() telegraf.Input {
+		return &AngieAPI{}
+	})
+}
